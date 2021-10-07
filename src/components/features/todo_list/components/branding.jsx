2eb997260e9a7cd1762cfb048489f4e262531337
MarcellusCodes <MarcellusCodes@gmail.com>
2021-10-07 14:20:09 +0200

    Removed Next Image and added html Image

--- src/components/features/todo_list/components/branding.jsx
+++ src/components/features/todo_list/components/branding.jsx
@@ -1,11 +1,10 @@
-import Image from "next/Image";
 import Logo from "../assets/logo.svg";
 
 const Branding = () => {
   return (
     <>
       <div className="flex flex-row items-center justify-center">
-        <Image src={Logo} alt="App Icon" width={72} height={72} />
+        <img src={Logo} alt="App Icon" width={72} height={72} />
         <h2
           className="text-6xl text-blue-500 font-bold"
           style={{ fontFamily: "Zeyada" }}
